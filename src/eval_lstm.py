75075f9d0bdf5be87a594f517e321cd039750a0c
iangonzalez <gonzalez.isv@gmail.com>
2023-02-18 17:51:19 -0800

    Output predictions for later analysis during eval.

--- src/eval_lstm.py
+++ src/eval_lstm.py
@@ -15,6 +15,7 @@
 # limitations under the License.
 
 import argparse
+import os
 import torch
 
 import set_config
@@ -27,6 +28,7 @@ def test_lstm(lstm, dataloader, cuda_device):
     lstm.eval()
     if cuda_device is not None:
         lstm.to(cuda_device)
+    all_preds = None
     with torch.no_grad():
         correct = 0.0
         total = 0.0
@@ -42,6 +44,12 @@ def test_lstm(lstm, dataloader, cuda_device):
 
             y = lstm(inputs)
 
+            # Accumulate predictions to return.
+            if all_preds is None:
+                all_preds = y.transpose(0,1)
+            else:
+                all_preds = torch.cat((all_preds, y.transpose(0,1)), dim=0)
+
             highest_predicted = torch.argmax(y, 2)
             actual_picked = torch.argmax(labels, 2)
 
@@ -50,9 +58,10 @@ def test_lstm(lstm, dataloader, cuda_device):
     
         accuracy = correct / total
         print("Validation accuracy:", accuracy, " Total picks:", int(total))
+    return all_preds
 
 
-def load_and_test_lstm(data_dir, model_path, test_on_gpu):
+def load_and_test_lstm(data_dir, model_path, test_on_gpu, preds_output_dir):
     cuda_device = None
     if test_on_gpu:
         if torch.cuda.is_available():
@@ -66,7 +75,11 @@ def load_and_test_lstm(data_dir, model_path, test_on_gpu):
         batch_size=1000
     )
     lstm = torch.load(model_path)
-    test_lstm(lstm, testloader, cuda_device)
+    preds = test_lstm(lstm, testloader, cuda_device)
+    if preds_output_dir:
+        preds_filename = "preds_for_model_{0}.pt".format(os.path.basename(model_path))
+        torch.save(preds, os.path.join(preds_output_dir, preds_filename))
+
 
 
 if __name__ == "__main__":
@@ -80,10 +93,13 @@ if __name__ == "__main__":
     parser.add_argument('--set-config-path', type=str, required=False, dest='set_config_path',
                         help='set config of the drafts being tested (configuration purposes). Overrides set-id.')
     parser.add_argument('--gpu', action='store_true')
+    parser.add_argument('-o', '--preds-output-dir',  type=str, dest='preds_output_dir', required=False,
+                        help='If set, saves the predictions in the given directory.')
     args = parser.parse_args()
     if args.set_config_path is not None:
         _SET_CONFIG = set_config.get_set_config_from_path(args.set_config_path)
     else:
         _SET_CONFIG = set_config.get_set_config(args.set_id)
 
-    load_and_test_lstm(data_dir=args.data_dir, model_path=args.model_path, test_on_gpu=args.gpu)
+    load_and_test_lstm(data_dir=args.data_dir, model_path=args.model_path,
+                       test_on_gpu=args.gpu, preds_output_dir=args.preds_output_dir)
